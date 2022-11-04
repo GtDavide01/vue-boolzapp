72d5352e36a39d630f7d1751df176fd1739100c6
GtDavide01 <davidefiorini01@gmail.com>
2022-11-04 12:13:35 +0100

    aggiunta possibilità di aggiungere nuovi contatti

--- js/script.js
+++ js/script.js
@@ -182,7 +182,8 @@ createApp({
             ],
             randomNumber : "",
             messageState : "",
-            dt: ""
+            dt: "",
+            userAvatar: "//cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png"
         }
     },
     methods:{
@@ -242,6 +243,17 @@ createApp({
             console.log(this.randomNumber)
             ;
         },
+        addChat: function(){
+            this.contacts.push({
+                name : prompt("Inserisci il nome della nuova chat"),
+                avatar : '_new',
+                visible: true, 
+                messages : [
+
+                ],
+
+            })
+        }
     },
     created(){
         this.messageState =  "Ultimo accesso "+this.contacts[this.thisChat].messages.slice(-1).pop().date;
